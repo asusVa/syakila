fc52298180cbf982ff2d3199ead4fe44db59173e
Ardhi Nur fauzi <asus.va60@gmail.com>
2024-11-28 14:21:01 +0700

    OKe

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+});
 
-// select toggle button
-let toggle = document.getElementById("mode");
-
-toggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-})
+// second toggle
+const switch_toggle = document.getElementById("switch_toggle");
+switch_toggle.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+});
